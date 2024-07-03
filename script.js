4d78df278a33f724b1f412c2cdaea16182d74005
SAGAR055 <sagarsuhaschavan367@gmail.com>
2024-07-03 10:09:41 +0530

    added age dynamic

--- script.js
+++ script.js
@@ -26,6 +26,34 @@ window.onclick = function(event) {
 
 
 
+//age calulator
+
+
+    var dob = new Date("04/28/2003");
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+    
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff); 
+    
+    //extract year from date    
+    var year = age_dt.getUTCFullYear();
+    
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+    
+    //display the calculated age
+
+    console.log(age);
+  let AgeDisplay = document.getElementById('ageDisplay');
+
+   AgeDisplay.innerHTML = age;
+   
+ 
+
+
+
+
 
 
 
