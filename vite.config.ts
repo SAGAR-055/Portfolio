615d0edd9ad8843aa5e67f84a37f2bde0c920f36
SAGAR CHAVAN <SAGARSUHASCHAVAN367@GMAIL.COM>
2026-07-07 09:16:54 +0000

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/Portfolio',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
