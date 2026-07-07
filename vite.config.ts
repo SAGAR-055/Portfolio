d24fb78b1b6ea4453c526c24e998686880d056a8
SAGAR CHAVAN <SAGARSUHASCHAVAN367@GMAIL.COM>
2026-07-07 13:11:54 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    base: '/Portfolio/',
   ],
   resolve: {
     alias: {
